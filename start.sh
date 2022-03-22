601b7715d6888e1f216dfb945e921757982c97cd
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2022-03-22 14:17:25 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Job initialized"
-cat $KERBEROS_KEYTAB | base64 > /tmp/krb5.keytab
+cat $KERBEROS_KEYTAB | base64 -d > /tmp/krb5.keytab
 kinit -kt /tmp/krb5.keytab $KERBEROS_USER
 echo "Job finished"
 sleep 3600
