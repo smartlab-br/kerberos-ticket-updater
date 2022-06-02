4d96d0320a5f70783bff3da6fc2b7b2014126a5a
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2022-06-02 18:38:31 -0400

    Reverting keytab to direct path + name

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo "Job initialized"
-echo "$KERBEROS_KEYTAB" | base64 -d > /tmp/krb5.keytab
-kinit -kt /tmp/krb5.keytab $KERBEROS_USER
+kinit -kt "$KERBEROS_PATH/$KERBEROS_KEYTAB" $KERBEROS_USER
 echo "Job finished"
