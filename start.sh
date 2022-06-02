ce42a4546efd3f5a282598af1e39c91d3ec0dd34
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2022-06-02 18:21:55 -0400

    Changing strategy from path to content.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Job initialized"
-cat $KERBEROS_KEYTAB | base64 -d > /tmp/krb5.keytab
+echo "$KERBEROS_KEYTAB" | base64 -d > /tmp/krb5.keytab
 kinit -kt /tmp/krb5.keytab $KERBEROS_USER
 echo "Job finished"
