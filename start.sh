1f225905d4134a466ce1ff78b2aadde3d01e5f0e
Charles Santos <thyarles@gmail.com>
2022-06-03 16:43:32 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 echo "Job initialized"
-kinit -kt "$KERBEROS_PATH/$KERBEROS_KEYTAB" $KERBEROS_USER
+KERBEROS_TICKET="$KERBEROS_PATH/$KERBEROS_KEYTAB"
+kinit -kt $KERBEROS_TICKET $KERBEROS_USER
+(( $? )) && exit 1
+chmod 644 $KERBEROS_TICKET
 echo "Job finished"
+exit 0
