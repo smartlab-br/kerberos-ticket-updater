f8c01d1fc0412f764e40300bb2dee707221d0916
Charles Santos <thyarles@gmail.com>
2022-06-03 17:19:37 -0300

    fix: Error check

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo "Job initialized"
 KERBEROS_TICKET="$KERBEROS_PATH/$KERBEROS_KEYTAB"
-kinit -Vkt $KERBEROS_TICKET $KERBEROS_USER
-[[ $? ]] && exit 1
+kinit -Vkt $KERBEROS_TICKET $KERBEROS_USER || exit 1
 chmod 644 $KRB5CCNAME
 echo "Job finished"
 exit 0
